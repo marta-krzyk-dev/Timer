9f4228288e0d3ff7e968555e3f35035a80d7745e
Marta Krzyk <marta-krzyk-dev@users.noreply.github.com>
2019-03-09 22:06:02 +0200

    Fix comments

--- Timer.h
+++ Timer.h
@@ -1,23 +1,25 @@
-public ref class Konkurs : public System::Windows::Forms::Form
+public ref class Timer : public System::Windows::Forms::Form
 { 			//Zmienne dla licznika czasu
- private:	int CzasLicznik; //Ilość sekund do końca odliczania
+ private:	
+ 
+ 			int CzasLicznik; //Ilość sekund do końca odliczania
  			int srednica, promien, szer, wys, lok_Y;
  			float kat;
  			float powiekszenie_kata;
  			PointF point_tekst;
 			SizeF rozmiar_lan;
 			
-			System::Drawing::Graphics^	g;
+			System::Drawing::Graphics^ g;
  			Rectangle prostokat;
 			static SolidBrush^ zielony = gcnew SolidBrush(Color::LimeGreen);
  			static SolidBrush^ zolty = gcnew SolidBrush(Color::FromArgb(227,217,15));
 			static SolidBrush^ pomarancz = gcnew SolidBrush(Color::DarkOrange);
 			static SolidBrush^ czerwony = gcnew SolidBrush(Color::Red); 			
-      static SolidBrush^ bialy = gcnew SolidBrush(Color::White);
-      static Pen^ pioro = gcnew Pen( Color::Black, 0.0f ); //Niewidoczne obramowanie
-      static Drawing::Font^ czcionka_timer;
+    			 static SolidBrush^ bialy = gcnew SolidBrush(Color::White);
+      			static Pen^ pioro = gcnew Pen( Color::Black, 0.0f ); //Niewidoczne obramowanie
+      			static Drawing::Font^ czcionka_timer;
 		
-	//Funkcja rysuje niepełne koło i napis z liczbą sek. pozostałych do końca odliczania 
+//Funkcja rysuje niepełne koło i napis z liczbą sek. pozostałych do końca odliczania 
 private: System::Void timerLicznik_Tick(System::Object^  sender, System::EventArgs^  e) {
 
 			if (CzasLicznik <= 0)
@@ -28,22 +30,22 @@ private: System::Void timerLicznik_Tick(System::Object^  sender, System::EventAr
 				 --CzasLicznik;
 				 kat += powiekszenie_kata; //Zwiększ kąt wycinka koła
 
-				g->Clear(panelZegar->BackColor); //Wyczyœæ narysowane w poprzedniej iteracji obiekty
+				g->Clear(panelZegar->BackColor); //Wyczyść tło
 
-			//Rysuj elipsê na polu prostok¹ta rect
+			//Rysuj elipsę na polu prostokąta rect
 			g->DrawEllipse(blackPen, rect);
 
-			//Wype³nij wycinek ko³a kolorem na podst. pozostałych sekund
-			if (CzasLicznik > 9) //Zosta³o 10,11...MAX sek.
-				g->FillPie( zielony, rect, 270.0F, kat); // Wype³nij na zielono
+			//Wypełnij wycinek koła kolorem na podst. pozostałych sekund
+			if (CzasLicznik > 9)
+				g->FillPie( zielony, rect, 270.0F, kat);
 			else
-				if (CzasLicznik > 6) //Zosta³o 7,8,9 sek.
-				g->FillPie( zolty, rect, 270.0F, kat); //Wype³nij na ¿ó³to
+				if (CzasLicznik > 6)
+				g->FillPie( zolty, rect, 270.0F, kat);
 				else
-					if (CzasLicznik > 3) //Zosta³o 4,5,6 sek.
-						g->FillPie( pomarancz, rect, 270.0F, kat);//Wype³nij na pomarañczowo
-					else	//Zosta³o 0,1,2,3 sekund
-						g->FillPie( czerwony, rect, 270.0F, kat); //Wype³nij na czerwono
+					if (CzasLicznik > 3)
+						g->FillPie( pomarancz, rect, 270.0F, kat);
+					else
+						g->FillPie( czerwony, rect, 270.0F, kat);
 
 			//Zmierz rozmiar napisu
 			rozmiar_lan = g->MeasureString( CzasLicznik.ToString(), drawFont);
